551df92d1337eb99055e68eaf261d34ca5eedf0c
Ryon Sherman <ryon.sherman@gmail.com>
2014-03-21 12:55:01 -0500

    Started porting working code

---
+++ libraries/FreshDesk.php
@@ -0,0 +1,105 @@
+<?php
+
+class FreshAPI
+{
+    protected function _request($url, $method = 'GET', $data = null)
+    {
+        
+    }
+}
+
+/**
+ * FreshDesk Forum Category
+ *
+ * Create, View, Update, and Delete Forum Categories.
+ *
+ * @link http://freshdesk.com/api/forums/forum-category
+ */
+class FreshForumCategory extends FreshAPI
+{
+    /**
+     * Create a new Forum Category.
+     * 
+     * Request URL: domain_URL/categories.json
+     * Request Method: POST
+     * 
+     * Request:
+     *     array = ('forum-category' => array(
+     *         'name'        => 'Test',                # required
+     *         'description' => 'New testing category' # optional
+     *     ));
+     * Response:
+     *     array = ('forum-category' => array(
+     *         'created-at'  => '2012-12-05T16:04:12+05:30',
+     *         'description' => 'New testing category',
+     *         'id'          => 2,
+     *         'name'        => 'Test',
+     *         'position'    => 2,
+     *         'updated-at'  => '2012-12-05T16:04:12+05:30'
+     *     ));
+     *
+     * @link http://freshdesk.com/api/forums/forum-category#create-a-forum-category
+     * 
+     * @param  string $name        Forum Category Name
+     * @param  string $description Forum Category Description
+     * @return array               API Response
+     */
+    protected function create($name, $description = '')
+    {
+        $data = array(
+            'name' => $name,
+            'description' => $description
+        );        
+        return $this->_request("categories.json", 'POST', $data);
+    }
+
+    protected function get($category_id = null)
+    {
+        if (intval($category_id)) $category_id = "/{$category_id}";
+        return $this->_request("categories{$category_id}.json")
+    }
+
+    protected function update($category_id, $name, $description = '')
+    {
+        $data = array(
+            'name' => $name,
+            'description' => $description,
+        );
+        return $this->_request("categories/{$category_id}.json", 'POST', $data);
+    }
+
+    protected function delete($category_id)
+    {
+        return $this->_request("categories/{$category_id}.json", 'DELETE');
+    }
+}
+
+class FreshForum extends FreshAPI
+{
+    public function __construct()
+    {
+        $this->Category = new FreshForumCategory();
+    }
+
+    protected function 
+    protected function topics($category_id, $forum_id)
+
+    protected function create();
+    protected function update();
+    protected function delete();
+}
+
+class FreshDesk extends FreshAPI
+{
+
+    private $CI;
+    protected $Forum
+
+    public function __construct()
+    {
+        $this->CI =& get_instance();
+        $this->Forum = new FreshForum();
+    }
+}
+
+?>
